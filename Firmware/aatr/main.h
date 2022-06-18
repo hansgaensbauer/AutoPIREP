2ebd4e41d8564e428b765237e9f9ff787e9bd4de
Hans Gaensbauer <htgaens@gmail.com>
2022-06-17 22:16:09 -0700

    Add Documentation and AATR Image

---
+++ Firmware/aatr/main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ * Created: 6/17/2022 10:03:56 PM
+ *  Author: hansg
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+	//project setup
+	#define USART_DEBUG
+
+
+
+#endif /* MAIN_H_ */
