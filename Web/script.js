3538441349b36209dbc241d4ae018f2f3dcb2bb5
Hans Gaensbauer <hans.gaensbauer@gmail.com>
2024-06-30 18:02:27 -0500

    Website working, preparing to switch map providers

--- Web/script.js
+++ Web/script.js
@@ -1,64 +1,125 @@
-// Initialize the map
-const map = L.map('map').setView([41.886398, -87.636688], 10); // Initial view set to a default location
+
+const map = L.map('map').setView([30.886398, -87.636688], 10);
+
+var points = [];
 
 // Set up the OSM layer
-var wmsLayer = L.tileLayer.wms('https://geoint.nrlssc.navy.mil/nrltileserver/wms/?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=FAASectionals&TILED=true&WIDTH=512&HEIGHT=512&SRS=EPSG%3A4326&STYLES=&BBOX=-112.5%2C22.5%2C-90%2C45', {
-    layers: 'FAASectionals',
-    format: 'image/png',
-    attribution: '&copy; <a href="https://geoint.nrlssc.navy.mil/#/">NRL</a>'
-}).addTo(map);
+document.addEventListener("DOMContentLoaded", function() {
 
-// Define the custom icon
-const customIcon = L.icon({
-    iconUrl: 'symbol.png', // Path to your custom icon
-    iconSize: [38, 38], // Size of the icon
-    iconAnchor: [22, 38], // Point of the icon which will correspond to marker's location
-    popupAnchor: [-3, -76] // Point from which the popup should open relative to the iconAnchor
+    // var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    //     subdomains: 'abcd',
+    //     maxZoom: 20
+    // }).addTo(map);
+    var wmsLayer = L.tileLayer.wms('https://geoint.nrlssc.navy.mil/nrltileserver/wms/?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=FAASectionals&TILED=true&WIDTH=512&HEIGHT=512&SRS=EPSG%3A4326&STYLES=&BBOX=-112.5%2C22.5%2C-90%2C45', {
+        layers: 'FAASectionals',
+        format: 'image/png',
+        opacity: 0.6,
+        attribution: '&copy; <a href="https://geoint.nrlssc.navy.mil/#/">NRL</a>'
+    }).addTo(map);
+
+    var toggleButton = document.getElementById('toggle-button');
+    var sidePanel = document.getElementById('side-panel');
+
+    toggleButton.addEventListener('click', function() {
+    sidePanel.classList.toggle('hidden');
+    });
+
+    //Get the points
+    //For now, let's just query all of the points in the US when we load
+    const gh = new Geohash();
+    const geohashPrefix = Geohash.encode(41.886, -87.636, 5).substring(0, 5);
+
+    queryEvents("d", 0, 6000);
+
 });
 
-// Hardcoded points for testing
-const points = [
-    { lat: 41.886, lon: -87.636, severity: 'Light', altitude: 6000, time: 4234, airframe: "Cessna 152"},
-    { lat: 41.886, lon: -87.736, severity: 'Light', altitude: 5600, time: 2334, airframe: "Cessna 172"  },
-    { lat: 41.685398, lon: -86.636288, severity: 'Med', altitude: 5600, time: 5634, airframe: "Cessna 172"  },
-    { lat: 41.7, lon: -88, severity: 'Light', altitude: 6100, time: 2343, airframe: "Piper J-3"  },
-];
+const pirep_light = L.icon({
+    iconUrl: 'Graphics/light.png',
+    iconSize: [35, 35],
+    iconAnchor: [22, 38],
+    popupAnchor: [-3, -76]
+});
 
-// Function to fetch points from the server
-async function fetchPoints() {
-    const response = await fetch('http://localhost:3000/api/points');
-    const points = await response.json();
-    return points;
+const pirep_moderate = L.icon({
+    iconUrl: 'Graphics/moderate.png',
+    iconSize: [35, 35],
+    iconAnchor: [22, 38],
+    popupAnchor: [-3, -76]
+});
+
+const pirep_severe = L.icon({
+    iconUrl: 'Graphics/severe.png',
+    iconSize: [35, 35],
+    iconAnchor: [22, 38],
+    popupAnchor: [-3, -76]
+});
+
+
+
+// Example usage:
+
+// const startTime = Math.floor(Date.now() / 1000) - 3600;  // Last hour
+// const endTime = Math.floor(Date.now() / 1000);
+
+// queryEvents(geohashPrefix, startTime, endTime);
+async function queryEvents(geohash, startTime, endTime) {
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://zohmomjv73.execute-api.us-east-1.amazonaws.com/prod/turbulence");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    const body = JSON.stringify({
+        "geohash": geohash,
+        "start_timestamp": startTime,
+        "end_timestamp": endTime
+    });
+    xhr.onload = () => {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        points = JSON.parse(xhr.response)["Items"];
+        console.log(points);
+        addPointsToMap();
+      } else {
+        console.log(`Error: ${xhr.status}`);
+      }
+    };
+    xhr.send(body);
 }
+
+
 
 // Function to add points to the map using the custom icon
 function addPointsToMap() {
+    console.log(points)
     const tableBody = document.getElementById('pointsTable').getElementsByTagName('tbody')[0];
     points.forEach(point => {
-        // Create a marker with the custom icon
-        const marker = L.marker([point.lat, point.lon], { icon: customIcon });
-        
-        // Bind a popup to the marker with additional information
-        marker.bindPopup(`
-            <b>Location:</b> ${point.lat}, ${point.lon}<br>
-            <b>Severity:</b> ${point.severity}
-        `);
-
-        // Add the marker to the map
-        marker.addTo(map);
 
         // Add the point to the table
         const row = tableBody.insertRow();
-        const cell_time = row.insertCell(0);
-        const cell_altitude = row.insertCell(1);
-        const cell_severity = row.insertCell(2);
-        const cell_airframe = row.insertCell(3);
-        cell_time.textContent = point.time;
-        cell_altitude.textContent = point.altitude;
-        cell_severity.textContent = point.severity;
-        cell_airframe.textContent = point.airframe;
+        const cell = row.insertCell(0);
+        
+        var marker = L.marker([point.latitude.N, point.longitude.N], { icon: pirep_severe });
+        console.log(point.severity.N)
+        switch(point.severity.N){
+            case "1": 
+                marker = L.marker([point.latitude.N, point.longitude.N], { icon: pirep_light }); 
+                cell.style.backgroundColor = "#b3ffca";
+                cell.textContent = "UA /TM " + point.timestamp.N + "/FL " + point.altitude.N + "/TP " + point.aircraft.S;
+                break;
+            case "3": 
+                marker = L.marker([point.latitude.N, point.longitude.N], { icon: pirep_moderate }); 
+                cell.style.backgroundColor = "#ffc56f";
+                cell.textContent = "UA /TM " + point.timestamp.N + "/FL " + point.altitude.N + "/TP " + point.aircraft.S;
+                break;
+            case "5": 
+                marker = L.marker([point.latitude.N, point.longitude.N], { icon: pirep_severe }); 
+                cell.style.backgroundColor = "#ffc6c6";
+                cell.textContent = "UUA /TM " + point.timestamp.N + "/FL " + point.altitude.N + "/TP " + point.aircraft.S;
+                break;
+            default:
+                console.log("default");
+        }
+
+        marker.bindPopup(cell.textContent);
+        marker.addTo(map);
     });
 }
-
-// Add points to the map
-addPointsToMap();
